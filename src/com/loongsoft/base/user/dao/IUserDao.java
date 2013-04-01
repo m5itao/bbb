259fbfa3ff90a703dd25afea3dffef9387d7b1a3
m5itao <icq7758955@sina.com>
2013-04-02 02:39:14 +0800

    Revert "init"
    This reverts commit 20af941c59e6ba0690e27529681fccea73373096.

--- src/com/loongsoft/base/user/dao/IUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.loongsoft.base.user.dao;
-
-import com.loongsoft.base.form.User;
-
-public interface IUserDao {
-	
-	public void add(User user);
-	
-}
--- src/com/loongsoft/base/user/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.loongsoft.base.user.service;
-
-import com.loongsoft.base.form.User;
-
-public interface IUserService {
-	
-	public void add(User user);
-	
-}
